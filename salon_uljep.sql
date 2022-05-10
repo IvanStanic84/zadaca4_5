4a42e316876d9ee7c62be72907ad6ecee13ec773
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-10 19:00:30 +0200

    Dodani tipovi podataka

--- salon_uljep.sql
+++ salon_uljep.sql
@@ -1,25 +1,36 @@
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\salon_uljep.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\salon_uljep.sql
 
 drop database if exists salon_uljep;
 create database salon_uljep;
 use salon_uljep;
 
 create table djelatnica(
-    ime varchar(50),
-    smjena varchar(50),
-    brojkorisnika varchar(50),
-    promet varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    smjena varchar(50) not null,
+    brojkorisnika int,
+    promet decimal (10,2)
 );
 
 create table korisnik(
+    sifra int not null primary key auto_increment,
     spol varchar(50),
     smjena varchar(50),
-    usluga varchar(50),
-    novac varchar(50)
+    usluga int not null,
+    novac decimal (10,2)
 );
 
 create table usluga (
-    vrsta varchar(50),
-    cijena varchar(50),
-    vrijeme varchar(50)
+    sifra int not null primary key auto_increment,
+    vrsta varchar(50) not null,
+    cijena decimal(10,2) not null,
+    vrijeme time
+);
+
+create table salon(
+    sifra int not null primary key auto_increment,
+    brojsalona int not null,
+    adresa varchar(50),
+    promet decimal(10,2),
+    djelatnica int not null 
 );
