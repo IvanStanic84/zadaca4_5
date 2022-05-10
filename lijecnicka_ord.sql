d31e033983146fc6f75d9381265a479d2b535452
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-10 19:22:19 +0200

    Dodani tipovi podataka

--- lijecnicka_ord.sql
+++ lijecnicka_ord.sql
@@ -1,4 +1,4 @@
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\lijecnicka_ord.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\lijecnicka_ord.sql
 
 drop database if exists lijecnicka_ord;
 create database lijecnicka_ord;
@@ -9,8 +9,27 @@ create table lijecnik(
     titula varchar(50),
     specijalizacija varchar(50)
 );
+
 create table ordinacija(
     vrstaord varchar(50),
     mjesto varchar(50),
-    adresa varchar(50)
+    adresa varchar(50),
+    lijecnik int not null
 );
+
+create table pacijent(
+    mbo char(9),
+    ime varchar(50),
+    prezime varchar(50),
+    pregled int not null
+);
+
+create table pregled(
+    vrsta varchar(50),
+    datum datetime,
+    vrijeme datetime,
+    lijecnik int not null,
+    cijena decimal(15,2)
+);
+
+
