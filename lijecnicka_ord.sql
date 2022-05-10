1df5a6d4022a2717ec674f254ca0cdb169d47b3e
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-10 19:28:58 +0200

    Dodani vanjski kljucevi

--- lijecnicka_ord.sql
+++ lijecnicka_ord.sql
@@ -5,31 +5,40 @@ create database lijecnicka_ord;
 use lijecnicka_ord;
 
 create table lijecnik(
-    ime varchar(50),
-    titula varchar(50),
-    specijalizacija varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    titula varchar(50)not null,
+    specijalizacija varchar(50) not null
 );
 
 create table ordinacija(
-    vrstaord varchar(50),
+    sifra int not null primary key auto_increment,
+    vrstaord varchar(50) not null,
     mjesto varchar(50),
     adresa varchar(50),
     lijecnik int not null
 );
 
 create table pacijent(
-    mbo char(9),
+    sifra int not null primary key auto_increment,
+    mbo char(9) not null,
     ime varchar(50),
     prezime varchar(50),
     pregled int not null
 );
 
 create table pregled(
-    vrsta varchar(50),
+    sifra int not null primary key auto_increment,
+    vrsta varchar(50) not null,
     datum datetime,
     vrijeme datetime,
     lijecnik int not null,
     cijena decimal(15,2)
 );
 
+# definiranje vanjskih ključeva
+
+alter table ordinacija add foreign key (lijecnik) references lijecnik(sifra);
+alter table pregled add foreign key (lijecnik) references lijecnik(sifra);
+alter table pacijent add foreign key (pregled) references pregled(sifra);
 
