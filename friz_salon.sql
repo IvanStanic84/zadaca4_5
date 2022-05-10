f26548ec39d5ff3d2fa627f825f087ea83d00660
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-10 10:23:21 +0200

    Update friz_salon.sql
    Promjena tipova podataka i (ne)poznavanja

--- friz_salon.sql
+++ friz_salon.sql
@@ -1,26 +1,36 @@
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\friz_salon.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\friz_salon.sql
 
 drop database if exists friz_salon;
 create database friz_salon;
 use friz_salon;
 
 create table djelatnica(
-    ime varchar(50),
-    smjena varchar(50),
-    brojkorisnika varchar(50),
-    promet varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    smjena varchar(50) not null,
+    brojkorisnika int,
+    promet decimal (10,2)
 );
 
 create table korisnik(
+    sifra int not null primary key auto_increment,
     spol varchar(50),
     smjena varchar(50),
-    usluga varchar(50),
-    novac varchar(50)
+    usluga varchar(50) not null,
+    novac decimal (10,2)
 );
 
 create table usluga (
-    vrsta varchar(50),
-    cijena varchar(50),
-    vrijeme varchar(50)
+    sifra int not null primary key auto_increment,
+    vrsta varchar(50) not null,
+    cijena decimal(10,2) not null,
+    vrijeme time
 );
 
+create table salon(
+    sifra int not null primary key auto_increment,
+    brojsalona int not null,
+    adresa varchar(50),
+    promet decimal(10,2),
+    djelatnica int not null 
+);
