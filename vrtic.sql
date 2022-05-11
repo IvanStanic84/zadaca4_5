c60a72cbcc9312ffe82b16445013d706892a4296
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-11 09:01:38 +0200

    Dodani tipovi podataka

--- vrtic.sql
+++ vrtic.sql
@@ -5,14 +5,33 @@ drop database if exists vrtic;
 create database vrtic;
 use vrtic;
 
+create table ustanova(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    adresa varchar(50),
+    ravnateljica varchar(50)
+);
+
 create table odgojnaskupina(
-    voditeljica varchar(50),
-    brojpolaznika varchar(50),
-    dobdjece varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    voditeljica int not null,
+    brojpolaznika int,
+    dobdjece varchar(50),
 );
 
 create table voditeljica(
-    ime varchar(50),
-    radnoiskustvo varchar(50),
+    sifra int not null primary key auto_increment,
+    imeprezime varchar(50),
+    radnoiskustvo datetime,
     strucnasprema varchar(50)
+);
+
+create table korisnik(
+    sifra int not null primary key auto_increment,
+    imeprezime varchar(),
+    oib char(11),
+    imeoca varchar(50),
+    imemajke varchar(50),
+    odgojnaskupina int not null
 );
