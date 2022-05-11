d98965b9548d888dbf7e4eaee8aac34575d3d8a6
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-11 10:06:19 +0200

    Dodani tipovi podataka

--- zastita_ziv.sql
+++ zastita_ziv.sql
@@ -1,19 +1,31 @@
 
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\zastita_ziv.sql
+# C:\xampp\mysql\bin\mysql -uroot  --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca4_5\zastita_ziv.sql
 
 drop database if exists zastita_ziv;
 create database zastita_ziv;
 use zastita_ziv;
 
+create table udruga(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    oib char(11),
+    adresa varchar(50),
+    iban varchar(50)
+);
+
 create table clanudruge(
+    sifra int not null primary key auto_increment,
     ime varchar(50),
-    brojsticenika varchar(50),
-    brojdolazaka varchar(50)
+    udruga int not null,
+    brojsticenika int,
+    mjesecnibrojdolazaka int
 );
 create table sticenik(
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     vrstaziv varchar(50),
-    prostor varchar(50)
+    prostor varchar(50),
+    udruga int not null
 );
 
 
