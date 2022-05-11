e8f72a50b089016bcce8557a6d284d7002f9dc96
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-11 10:24:39 +0200

    Unesene vrijednosti

--- zastita_ziv.sql
+++ zastita_ziv.sql
@@ -7,22 +7,22 @@ use zastita_ziv;
 
 create table udruga(
     sifra int not null primary key auto_increment,
-    naziv varchar(50),
-    oib char(11),
+    naziv varchar(50) not null,
+    oib char(11) not null,
     adresa varchar(50),
     iban varchar(50)
 );
 
 create table clanudruge(
     sifra int not null primary key auto_increment,
-    ime varchar(50),
+    ime varchar(50) not null,
     udruga int not null,
     brojsticenika int,
     mjesecnibrojdolazaka int
 );
 create table sticenik(
     sifra int not null primary key auto_increment,
-    ime varchar(50),
+    ime varchar(50) not null,
     vrstaziv varchar(50),
     prostor varchar(50),
     udruga int not null
@@ -32,3 +32,14 @@ create table sticenik(
 
 alter table clanudruge add foreign key (udruga) references udruga(sifra);
 alter table sticenik add foreign key (udruga) references udruga(sifra);
+
+# unos podataka
+
+insert into udruga (naziv, oib)
+values ('9 zivota', 55533399911);
+
+insert into clanudruge (ime, udruga)
+values ('Sofija', 1);
+
+insert into sticenik (ime, udruga)
+values ('Floki', 1);
